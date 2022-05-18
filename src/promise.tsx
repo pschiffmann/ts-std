2e485ccecee97b273168d8ad93fdc3b3b117fc14
Philipp Schiffmann <philippschiffmann93@gmail.com>
2022-05-18 10:05:26 +0200

    Rename `create-promise.tsx` to `promise.tsx`

--- src/promise.tsx
+++ src/promise.tsx
@@ -5,11 +5,20 @@ export type Reject = (reason?: any) => void;
  * Creates a new `Promise`. Returns the promise along with the `resolve` and
  * `reject` functions that are passed to the constructor callback.
  */
-export function createPromise<T = void>(): [Promise<T>, Resolve<T>, Reject] {
+export function create<T = void>(): [Promise<T>, Resolve<T>, Reject] {
   let _resolve: Resolve<T>, _reject: Reject;
   const promise = new Promise<T>((resolve, reject) => {
     _resolve = resolve;
     _reject = reject;
   });
   return [promise, _resolve!, _reject!];
+}
+
+/**
+ * Returns a new promise that succeeds after `ms` milliseconds.
+ */
+export function wait(ms: number): Promise<void> {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
 }
