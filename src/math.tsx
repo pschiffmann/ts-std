5d3aec88094e21f3dde29d0cd31dbca453abe657
Philipp Schiffmann <philippschiffmann93@gmail.com>
2022-06-15 14:40:46 +0200

    Add $Math.getRandomInt()

--- src/math.tsx
+++ src/math.tsx
@@ -7,3 +7,14 @@ export function sum(values: number[]): number {
   for (const n of values) result += n;
   return result;
 }
+
+/**
+ * Returns a random integer from range [min, max).
+ *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_integer_between_two_values
+ */
+export function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
